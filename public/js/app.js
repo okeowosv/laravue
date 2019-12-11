ebc20cca3e1ab808cd3ba1bb8981ccc183bc7e41
okeowosv <okeowoonline@gmail.com>
2019-12-11 14:18:02 +0100

    Detect active menu & disable url refresh

--- public/js/app.js
+++ public/js/app.js
@@ -52517,6 +52517,7 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 
 Vue.use(vue_router__WEBPACK_IMPORTED_MODULE_0__["default"]);
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_0__["default"]({
+  mode: 'history',
   routes: _routes_js__WEBPACK_IMPORTED_MODULE_1__["default"]
 });
 /**
--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,7 @@ import Routes from './routes.js'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+    mode: 'history',
     routes : Routes
 })
 
